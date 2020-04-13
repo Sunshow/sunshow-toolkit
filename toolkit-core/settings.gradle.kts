958bcf2d9be0b9e5950a3353aa81dea5c8584428
Sunshow <sunshow@gmail.com>
2020-04-13 13:50:38 +0800

    split java and kotlin from one project

--- toolkit-core/settings.gradle.kts
+++
@@ -1,9 +0,0 @@
-rootProject.name = "toolkit-core"
-include("core-enums")
-include("core-base")
-include("core-utils")
-include("core-qbean-api")
-include("core-qbean-helper")
-include("core-qbean-processor")
-include("core-qbean-sample")
-
---
+++ toolkit-kotlin/settings.gradle.kts
@@ -0,0 +1,4 @@
+rootProject.name = "toolkit-kotlin"
+include("core-base")
+include("core-enums")
+
