e90389d177de48928739cf31e12153ef628dc828
Sunshow <sunshow@gmail.com>
2024-10-24 22:16:29 +0800

    修复软删除

--- toolkit-core/core-qbean-helper/src/main/java/net/sunshow/toolkit/core/qbean/helper/repository/BaseRepositoryFactoryBean.java
+++ toolkit-core/core-qbean-helper/src/main/java/net/sunshow/toolkit/core/qbean/helper/repository/BaseRepositoryFactoryBean.java
@@ -2,9 +2,10 @@ package net.sunshow.toolkit.core.qbean.helper.repository;
 
 import jakarta.persistence.EntityManager;
 import net.sunshow.toolkit.core.qbean.helper.repository.impl.BaseExtRepositoryImpl;
+import nxcloud.foundation.core.data.jpa.repository.support.AdvancedJpaRepositoryFactory;
+import nxcloud.foundation.core.data.jpa.repository.support.AdvancedJpaRepositoryFactoryBean;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.support.JpaRepositoryFactory;
-import org.springframework.data.jpa.repository.support.JpaRepositoryFactoryBean;
 import org.springframework.data.repository.core.RepositoryMetadata;
 import org.springframework.data.repository.core.support.RepositoryFactorySupport;
 
@@ -14,25 +15,27 @@ import java.io.Serializable;
  * @author sunshow
  */
 public class BaseRepositoryFactoryBean<R extends JpaRepository<T, I>, T,
-        I extends Serializable> extends JpaRepositoryFactoryBean<R, T, I> {
+        I extends Serializable> extends AdvancedJpaRepositoryFactoryBean<R, T, I> {
 
     public BaseRepositoryFactoryBean(Class<? extends R> repositoryInterface) {
         super(repositoryInterface);
     }
 
-    protected RepositoryFactorySupport createRepositoryFactory(EntityManager em) {
-        return new BaseRepositoryFactory(em);
+    @NotNull
+    protected RepositoryFactorySupport createRepositoryFactory(@NotNull EntityManager em) {
+        return new BaseRepositoryFactory<>(em);
     }
 
     private static class BaseRepositoryFactory<T, I extends Serializable>
-            extends JpaRepositoryFactory {
+            extends AdvancedJpaRepositoryFactory {
 
         public BaseRepositoryFactory(EntityManager em) {
             super(em);
         }
 
+        @NotNull
         @Override
-        protected Class<?> getRepositoryBaseClass(RepositoryMetadata metadata) {
+        protected Class<?> getRepositoryBaseClass(@NotNull RepositoryMetadata metadata) {
             return BaseExtRepositoryImpl.class;
         }
     }
